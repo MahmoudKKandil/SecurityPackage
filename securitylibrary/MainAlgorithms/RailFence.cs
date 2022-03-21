7c50ccd27a74ba0c7dd6707c0de863423b0a41a2
youssefgamal111 <youssefuture@outlook.com>
2022-03-21 19:27:05 +0200

    impl.railfence enc and dec

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -15,12 +15,41 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            string plainText = "";
+            int nextIndex = (cipherText.Length + key - 1) / key;
+            int count = 0;
+            while(count<nextIndex)
+            {
+                int hold = count;
+                for (int j = 0; j < key; j++)
+                {
+                    if (count >= cipherText.Length) { break; }
+
+                    plainText += cipherText[count];
+                    count += nextIndex;
+                }
+                count = hold + 1;
+                
+
+            }
+
+            return plainText;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            string cipher = "";
+            int count = 0;
+            int secCount = 0;
+            for(int i = 0; i <plainText.Length; i++)
+            {
+                if (count >= plainText.Length) { secCount++;count = secCount; }
+                    cipher+= plainText[count];
+                    count += key;
+                
+
+            }
+            return cipher;
         }
     }
 }
