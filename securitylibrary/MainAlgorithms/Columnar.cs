f1a9d8c53d0047588aecb31285747cf7d3f17dfc
youssefgamal111 <youssefuture@outlook.com>
2022-03-22 03:36:31 +0200

    coloumnar enc

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -16,11 +16,41 @@ namespace SecurityLibrary
         public string Decrypt(string cipherText, List<int> key)
         {
             throw new NotImplementedException();
+
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
-        }
+
+            string cipher = "";
+
+            int noOfRows = (plainText.Length + key.Count - 1) / key.Count;
+            int noOfCols = key.Count;
+            int count = 0;
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            for (int i = 0; i < key.Count; i++)
+            {
+                int index = key.FindIndex(a => a.Equals(i + 1));
+                d.Add(i, index);
+            }
+
+            foreach (var col in d)
+            {
+                int n = col.Value;
+                for (int j = 0; j < noOfRows; j++)
+                {
+                    if (n >= plainText.Length) { break; }
+                    cipher += plainText[n];
+
+                    n += noOfCols;
+
+                }
+                count++;
+            }
+
+            return cipher;
+
+        
+    }
     }
 }
